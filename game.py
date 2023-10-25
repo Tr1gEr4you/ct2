db8f37e4e057501f7c229f24d95b1eeb3a185e65
Tr1gEr <73477492+Tr1gEr4you@users.noreply.github.com>
2023-10-25 17:01:03 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,73 @@
+import random
+
+# Получение случайного слова из списка
+def get_word():
+    word_list = ["олень", "слон", "жираф", "кенгуру", "тигр", "леопард", "зебра"]
+    return random.choice(word_list)
+
+# Создание таблицы с отображением угаданных букв
+def create_table(word):
+    return ["\u25A0" for _ in word]
+
+# Получение количества "жизней"
+def get_lives(word):
+    return len(word)
+
+# Проверка, жив ли игрок (остались ли "жизни")
+def is_alive(lives):
+    return lives > 0
+
+# Отображение текущей таблицы с угаданными буквами
+def show_table(table):
+    display = " ".join(table)
+    print(display)
+
+# Получение ответа от игрока (буквы или слова)
+def get_player_input():
+    return input("Угадайте букву или введите слово целиком: ").lower()
+
+# Проверка, является ли ответ правильным словом
+def is_word_correct(word, answer):
+    return word == answer
+
+# Проверка, является ли буква правильной
+def is_letter_correct(word, letter):
+    return letter in word
+
+# Обновление таблицы с угаданными буквами
+def update_table(word, table, letter):
+    for i, char in enumerate(word):
+        if char == letter:
+            table[i] = letter
+
+# Основная логика игры
+def play_hangman():
+    word = get_word()
+    table = create_table(word)
+    lives = get_lives(word)
+    
+    print("Добро пожаловать в игру 'Виселица на поле чудес'!")
+    print("У вас есть", lives, "жизней.")
+    
+    while is_alive(lives):
+        show_table(table)
+        answer = get_player_input()
+        
+        if len(answer) == 1:
+            if is_letter_correct(word, answer):
+                print("Правильно! Буква", answer, "находится в слове.")
+                update_table(word, table, answer)
+                if "".join(table) == word:
+                    print("Поздравляем! Вы угадали слово:", word)
+                    break
+            else:
+                print("Неправильно! Буквы", answer, "нет в слове.")
+                lives -= 1
+        elif is_word_correct(word, answer):
+            print("Поздравляем! Вы угадали слово:", word)
+            break
+        else:
+            print("Неправильный ввод. Пожалуйста, введите одну букву или слово целиком.")
+    
+    if lives == 0:
+        print("Игра окончена. Загаданное слово было:", word)
